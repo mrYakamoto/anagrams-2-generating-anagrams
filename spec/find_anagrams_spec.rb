14c724ac8bd3903381637552a595401b017da3b5
Torey Hickman <torey@toreyhickman.com>
2015-09-09 14:33:59 -0400

    Refactor tests
    Remove empty test examples, so that students can write them themselves.

--- spec/find_anagrams_spec.rb
+++ spec/find_anagrams_spec.rb
@@ -1,20 +1,10 @@
 require_relative '../find_anagrams'
 
-describe 'anagrams_for' do
-  let(:dictionary) {['acres', 'cares', 'Cesar', 'races', 'smelt', 'melts', 'etlsm', 'shazam']}
+describe 'find_anagrams' do
+  it 'returns any anagrams from the provided dictionary' do
+    possible_anagrams = ['nighthawks', 'caret', 'react', 'trace', 'otters']
+    found_anagrams    = find_anagrams('crate', possible_anagrams)
 
-  it "should detect anagrams in a dictionary" do
-    expect(anagrams_for('smelt', dictionary)).to eq(['smelt', 'melts', 'etlsm'])
+    expect(found_anagrams).to match_array ['caret', 'react', 'trace']
   end
-
-  it "should return the input word if it's in the dictionary" do
-    raise "Test needed!"
-  end
-
-  it "should detect anagrams in a dictionary even if the input is not" do
-    raise "Test needed!"
-  end
-
-  #You'll need to add more tests. Look to the "Guidelines" section for
-  # inspiration of you can't think of anything.
 end
